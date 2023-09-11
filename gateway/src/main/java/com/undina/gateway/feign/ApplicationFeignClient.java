9a708eec091280420430833ca2e9618b33b9f231
undina <foxundina@gmail.com>
2023-09-11 16:44:40 +0300

    openapi, feign

---
+++ gateway/src/main/java/com/undina/gateway/feign/ApplicationFeignClient.java
@@ -0,0 +1,18 @@
+package com.undina.gateway.feign;
+
+
+import org.openapitools.model.LoanApplicationRequestDTO;
+import org.openapitools.model.LoanOfferDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(url = "${feign.application.url}", name = "APPLICATION-FEIGN-CLIENT")
+public interface ApplicationFeignClient {
+    @PostMapping()
+    ResponseEntity<Void> createApplication(@RequestBody LoanApplicationRequestDTO loanApplicationRequestDTO);
+
+    @PostMapping("/offer")
+    ResponseEntity<Void> sendLoanOffer(@RequestBody LoanOfferDTO loanOfferDTO);
+}
